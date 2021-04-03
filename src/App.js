cba5857bd66fd03c7b0988bfd8ea6dab185a1f49
Gary OConnor <garyoconnor50@gmail.com>
2021-04-03 10:46:51 +0100

    basic structure of page links created. Styling began on homepage

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import "./App.css";
 import Nav from "./Components/Nav";
 import { Router } from "@reach/router";
 import Topics from "./Components/Topics";
+import Homepage from "./Components/Homepage";
+import Articles from "./Components/Articles";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Account from "./Components/Account";
 
 function App() {
   return (
     <main className="main-app">
       <Nav />
       <Router>
+        <Homepage path="/" />
         <Topics path="/topics" />
+        <Articles path="/articles" />
+        <Login path="/login" />
+        <Signup path="/signup" />
+        <Account path="/account" />
       </Router>
     </main>
   );
---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Homepage(props) {
+  return (
+    <section className="homepage-section">
+      <header className="homepage-header">
+        <h1 className="homepage-title">NC-News</h1>
+        <h2 className="homepage-subtitle">Where News Happens</h2>
+        <p className="homepage-desc">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum alias
+          perspiciatis nisi voluptatem mollitia, illo eligendi harum repudiandae
+          unde eos error labore deleniti sapiente officiis explicabo repellendus
+          modi! Dolores, earum!
+        </p>
+      </header>
+      <main className="homepage-articles">
+        <article className="homepage-article">
+          <h3 className="homepage-article-title">Title Example</h3>
+          <h4 className="homepage-article-topic">Topic Example</h4>
+          <p className="homepage-article-body">
+            Body Example. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Nam quam quod in, nostrum sequi ipsa soluta doloribus velit
+            totam itaque nulla et dolorem qui quo iste vero labore similique
+            adipisci!
+          </p>
+          <h4 className="homepage-article-author">Author Example</h4>
+          <h5 className="homepage-article-posted">Posted at Example</h5>
+        </article>
+      </main>
+    </section>
+  );
+}
+
+export default Homepage;
